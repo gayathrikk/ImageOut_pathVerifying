f7532ae1fe34bd27bdebfb6d05e794bc2b2e810f
Divya <annotation.divya@gmail.com>
2025-01-08 15:45:16 +0530

    Initial commit with Imageout_pathVerify.java

---
+++ src/main/java/Automation/test/App.java
@@ -0,0 +1,13 @@
+package Automation.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
